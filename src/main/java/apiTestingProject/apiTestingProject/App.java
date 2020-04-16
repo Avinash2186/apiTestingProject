7dca5d90fec7f41429ed4115606ca28ae230f48b
Avinash Motwani <avinash2186@gmail.com>
2020-04-16 22:12:48 +0530

    first commit

---
+++ src/main/java/apiTestingProject/apiTestingProject/App.java
@@ -0,0 +1,13 @@
+package apiTestingProject.apiTestingProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/testCases/TC_1_GetAllUsers.java
@@ -0,0 +1,53 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+import java.util.HashMap;
+
+public class TC_1_GetAllUsers {
+	String userId = "6";
+	
+	@Test(priority = 3)
+	public void test_getAllUsers_Page1() {
+		given()
+		.when().get("https://reqres.in/api/users")
+		.then().statusCode(200)
+		.log().body();	
+	}
+	
+	@Test (priority = 1)
+	public void test_CreateUser() {
+		HashMap data = new HashMap();
+		data.put("name", "Avinash");
+		data.put("job", "Automation Lead");
+		
+		Response res = given()
+		.contentType("application/json")
+		.body(data)
+		.when()
+		.post("https://reqres.in/api/users")
+		.then()
+		.statusCode(201)
+		.log().body()
+		.extract().response();
+		
+		System.out.println("=======================================/n \n ");
+		String responseBodyAsString = res.asString();
+		System.out.println("responseBodyAsString===>"+responseBodyAsString);
+		
+	}
+	
+	@Test(priority = 2)
+		public void test_getNewlyCreatedUser() {
+			given()
+			.when().get("https://reqres.in/api/users/"+userId)
+			.then().statusCode(200)
+			.log().body().extract();	
+		}
+
+}
